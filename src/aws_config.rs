3deafaf3da81d4514903762678aedaf9f0604190
Martin Kemp <martin@foxandducksoftware.co.uk>
2020-05-05 09:19:58 +0100

    fix tests on windows

--- src/aws_config.rs
+++ src/aws_config.rs
@@ -135,13 +135,13 @@ mod tests {
     use super::{write_credentials, AWSConfig};
     use ini::Ini;
     use std::collections::HashMap;
+    use std::env;
     use std::fs::{remove_file, File};
-    use std::path::PathBuf;
 
     const TEST_PROFILE: &str = "test";
     const TEST_AK: &str = "ThisIsAnAccessKey";
     const TEST_SK: &str = "ThisIsASecretKey";
-    const TEST_CRED_PATH: &str = "/tmp/test-credentials";
+    const TEST_CRED_PATH: &str = "test-credentials";
 
     #[test]
     fn aws_config() {
@@ -154,15 +154,21 @@ mod tests {
     fn write_test_credentials() {
         let mut config = HashMap::<String, AWSConfig>::new();
         config.insert(TEST_PROFILE.to_string(), AWSConfig::new(&TEST_AK, &TEST_SK));
-        let temp_path = PathBuf::from(TEST_CRED_PATH);
-        File::create(TEST_CRED_PATH).unwrap();
+
+        let mut temp_path = env::current_dir().unwrap();
+        temp_path.push(TEST_CRED_PATH);
+
+        File::create(&temp_path).unwrap();
         write_credentials(config, &temp_path).unwrap();
-        let ini_file = Ini::load_from_file(TEST_CRED_PATH).unwrap();
+
+        let ini_file = Ini::load_from_file(&temp_path).unwrap();
         assert!(!ini_file.is_empty());
         assert!(ini_file.section(Some(TEST_PROFILE)).is_some());
+
         let test_section = ini_file.section(Some(TEST_PROFILE)).unwrap();
         assert_eq!(test_section.get("aws_access_key_id").unwrap(), TEST_AK);
         assert_eq!(test_section.get("aws_secret_access_key").unwrap(), TEST_SK);
+
         //cleanup
         remove_file(TEST_CRED_PATH).unwrap()
     }

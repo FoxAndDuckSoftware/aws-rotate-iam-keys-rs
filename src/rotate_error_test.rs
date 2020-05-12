63947ab7d11ee090cc2c9c005e44c1e8f258112d
Martin Kemp <me@martinke.mp>
2020-05-12 22:45:05 +0100

    fix build

--- src/rotate_error_test.rs
+++ src/rotate_error_test.rs
@@ -5,5 +5,5 @@ const TEST_ERROR: &str = "Hello, World!";
 #[test]
 fn new_rotate_error() {
     let e = RotateError::new(&TEST_ERROR);
-    assert_eq!("Hello, World!", format!(e))
+    assert_eq!("Hello, World!", format!("{}", e))
 }

a8a1c7f04a903ba8ee1cbbb1870ce9cefc3cf6d6
Martin Kemp <me@martinke.mp>
2020-11-11 11:15:26 +0000

    Update docs
    Signed-off-by: Martin Kemp <me@martinke.mp>

--- docs/api/implementors/core/convert/trait.From.js
+++ docs/api/implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["rotate_iam_keys"] = [{"text":"impl Debug for AWSConfig","synthetic":false,"types":[]},{"text":"impl Debug for RotateError","synthetic":false,"types":[]}];
+implementors["rotate_iam_keys"] = [{"text":"impl&lt;E:&nbsp;Error + 'static&gt; From&lt;RusotoError&lt;E&gt;&gt; for RotateError","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
